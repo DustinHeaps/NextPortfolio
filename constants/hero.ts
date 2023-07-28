b8c966fa4971886abf5b23570b505b93ea592a4f
DustinHeaps <60202144+DustinHeaps@users.noreply.github.com>
2023-07-27 21:41:18 -0400

    capitalized js

--- constants/hero.ts
+++ constants/hero.ts
@@ -1,2 +1,2 @@
 export const heroText: string =
-  "I am a Web Developer with 4 years of experience in the industry. My focus area for the past few years has been frontend development with React, utilizing TypeScript. I am also skilled in backend development with Node.js and have experience using TypeScript in that context as well.";
+  "I am a Web Developer with 4 years of experience in the industry. My focus area for the past few years has been frontend development with React, utilizing TypeScript. I am also skilled in backend development with Node.JS and have experience using TypeScript in that context as well.";
