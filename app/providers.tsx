eeeea69e12063f6f09f95f4f12c5c4f30e67c84e
DustinHeaps <60202144+DustinHeaps@users.noreply.github.com>
2024-10-25 13:56:34 -0400

    minor change

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,7 +5,7 @@ import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: "https://dustinheaps.com/ingest",
+    api_host: "/ingest",
     ui_host: "https://us.posthog.com",
   });
 }
