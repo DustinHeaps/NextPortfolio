b973bca389272fe994a3a888fdc65320f2d2c795
DustinHeaps <60202144+DustinHeaps@users.noreply.github.com>
2025-06-02 12:17:36 -0400

    updated resume link and added waivify project

--- constants/index.ts
+++ constants/index.ts
@@ -7,7 +7,7 @@ import { navLinks } from "./navLinks";
 import { testimonials } from "./testimonials";
 
 export const resumeLink: string =
- "https://drive.google.com/file/d/1meP-fkNowLUf1FASlrTyExyV9DhU95ew/view?usp=sharing";
+ "https://drive.google.com/file/d/1K7AvmzS0pk7Vsw4FzbRKxPYRtlg5ky7g/view?usp=drive_link";
 
 export const gmailLink: string = "dustinheaps89@gmail.com";
 
--- constants/projects.ts
+++ constants/projects.ts
@@ -11,6 +11,21 @@ export interface ProjectProps {
 export const projects: ProjectProps[] = [
   {
     id: 1,
+    title: "Waivify",
+    image: "/assets/images/waivify.png",
+    description: `A digital waiver management app for solo service providers and small businesses. Users can collect signatures, manage submissions, and export signed waivers with ease. The dashboard includes filtering, CSV export, and branding tools—no coding required.`,
+    technologies: [
+      "NextJS 15",
+      "Stripe",
+      "TypeScript",
+      "Tailwind CSS",
+      "Clerk",
+    ],
+    projectLink: "https://waivify.com",
+    githubLink: "https://github.com/DustinHeaps/waivify-app",
+  },
+  {
+    id: 2,
     title: "Buildr",
     image: "/assets/images/buildr.png",
     description: `This modern website builder app revolutionizes web development by combining an intuitive drag-and-drop interface with robust design tools, making it easy for anyone to create stunning websites without any coding knowledge. In addition to powerful website creation capabilities, the app features an integrated Kanban board, providing a seamless project management experience.`,
@@ -25,7 +40,7 @@ export const projects: ProjectProps[] = [
     githubLink: "https://github.com/DustinHeaps/Buildr",
   },
   {
-    id: 2,
+    id: 3,
     title: "DevOverflow",
     image: "/assets/images/devoverflow.png",
     description: `An open forum dedicated to creating the ultimate compendium of programming queries and solutions. This collaborative venue serves as a hub for solving technical puzzles, contributed to and utilized by a global community. It ranks among the world's most frequented digital destinations for developers and tech enthusiasts`,
@@ -39,19 +54,19 @@ export const projects: ProjectProps[] = [
     projectLink: "https://dev-overflow-beta.vercel.app/",
     githubLink: "https://github.com/DustinHeaps/DevOverflow",
   },
-  {
-    id: 3,
-    title: "Vacations.com",
-    image: "/assets/images/vacations.png",
-    description: `Discover the perfect blend of comfort, convenience, and adventure with Vacations.com, the premier online destination for hotel bookings that transforms every trip into an unforgettable experience. Whether you're planning a serene getaway, a business trip, or a thrilling adventure, our extensive collection of accommodations ensures you find the perfect stay that caters to your every need.`,
-    technologies: [
-      "NextJs 14",
-      "TypeScript",
-      "Tailwind CSS",
-      "Next-Auth",
-      "Stripe",
-    ],
-    projectLink: "https://booking-app-ten-blush.vercel.app/",
-    githubLink: "https://github.com/DustinHeaps/BookingApp",
-  },
+  // {
+  //   id: 3,
+  //   title: "Vacations.com",
+  //   image: "/assets/images/vacations.png",
+  //   description: `Discover the perfect blend of comfort, convenience, and adventure with Vacations.com, the premier online destination for hotel bookings that transforms every trip into an unforgettable experience. Whether you're planning a serene getaway, a business trip, or a thrilling adventure, our extensive collection of accommodations ensures you find the perfect stay that caters to your every need.`,
+  //   technologies: [
+  //     "NextJs 14",
+  //     "TypeScript",
+  //     "Tailwind CSS",
+  //     "Next-Auth",
+  //     "Stripe",
+  //   ],
+  //   projectLink: "https://booking-app-ten-blush.vercel.app/",
+  //   githubLink: "https://github.com/DustinHeaps/BookingApp",
+  // },
 ];
